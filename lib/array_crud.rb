a154f22d28ba39b5e9cc0c908e2f7daad6f7fc3b
Joaquim Barreto <joaquim@barreto.co>
2018-10-28 16:55:16 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,23 +3,23 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [1, 2, 3, 4]
+  array = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(5)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(6)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop(7)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift(8)
 end
 
 def retrieve_element_from_index(array, index_number)
